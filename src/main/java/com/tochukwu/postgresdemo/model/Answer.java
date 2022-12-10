a9c48769c4dc8cbb50a793a1af437a612f6b7c18
TOCHUKWU-AGADA <tochukwuagada26@gmail.com>
2022-12-10 18:33:32 +0100

    again

---
+++ src/main/java/com/tochukwu/postgresdemo/model/Answer.java
@@ -0,0 +1,62 @@
+package com.tochukwu.postgresdemo.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "answers")
+public class Answer extends AuditModel{
+    @jakarta.persistence.Id
+    @GeneratedValue(generator = "answer_generator")
+    @SequenceGenerator(
+            name = "answer_generator",
+            sequenceName = "answer_sequence",
+            initialValue = 1000
+    )
+    private Long Id;
+
+    @Column(columnDefinition = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "question_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Question question;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "Id=" + Id +
+                ", text='" + text + '\'' +
+                ", question=" + question +
+                '}';
+    }
+}
---
+++ src/test/java/com/tochukwu/postgresdemo/PostgresDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tochukwu.postgresdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
